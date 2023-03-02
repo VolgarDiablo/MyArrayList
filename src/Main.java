9e7a1da50c3d59f197df554fc278eaaf14c7c333
Антон Дидковский <didkovskiy.anton@gmail.com>
2023-03-03 00:16:31 +0200

    done

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("!");
         MyArrayList<Integer> list = new MyArrayList<>();
 
-        list.add(1);
-        list.add(2);
+        for (int i = 0; i < 10; i++) list.add(i);
+        System.out.println(list);
+        System.out.println("size: " + list.size() + "\n");
+
+        list.add(10);
+        System.out.println(list.get(10));
+        System.out.println(list);
+        System.out.println(list.remove(5) + "\n");
 
         System.out.println(list);
+
+        list.clear();
+        System.out.println("clear: " + list);
+
     }
 }
--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
-// <L>?!
-public class MyArrayList<L> {
+// <E>?!
+public class MyArrayList<E> {
     private Object[] array;
     private int size;
     private final static int DEFAULT_CAPACITY = 10;
@@ -12,15 +12,43 @@ public class MyArrayList<L> {
     }
 
     public void add(Object element) {
+        if (size == array.length) resize();
         array[size++] = element;
+    }
+
+    public Object remove(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        Object removedObject = array[index];
+        array[index] = null;
+        System.arraycopy(array, index + 1, array, index, size - index);
+        return removedObject;
+    }
+
+    public void clear() {
+//        for (int i = 0; i < size; i++) {
+//            array[i] = null;
+//        }
+        size = 0;
+        array = new Object[DEFAULT_CAPACITY];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Object get(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        return array[index];
+    }
+
+    private void resize() {
+        Object[] newArray = new Object[array.length * 3 / 2];
+        System.arraycopy(array, 0, newArray, 0, size);
+        array = newArray;
     }
 
     @Override
     public String toString() {
         return Arrays.toString(array);
-    }
-
-    public int size() {
-        return size;
     }
 }
